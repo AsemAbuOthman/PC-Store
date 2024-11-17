1c7b3cf1bef095296c606b9196df2fa5ed4f8d2f
Asem Abu Othman <96397277+AsemAbuOthman@users.noreply.github.com>
2024-11-17 19:09:17 +0300

    Add files via upload

---
+++ JS Files/JS1.js
@@ -0,0 +1,5 @@
+window.alert("My Name is ");
+window.alert("Asem Ibrahim Mohammad AbuOthman");
+var visit;
+visit = (window.prompt("Enter Your Name : " ));
+document.write("<p>The Visitor Name is : " + visit + "</p>");
